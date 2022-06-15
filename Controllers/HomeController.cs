c437b7781aede05a2cc7454a8acdd3c62c50e448
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-15 14:03:20 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using BudgetCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace BudgetCalculator.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var model = new BudgetModel() { Description = "Hello", Value = 500 };
+			return View(model);
+		}
+
+		[HttpPost]
+		public IActionResult Index(BudgetModel model)
+		{
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Models/BudgetModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BudgetCalculator.Models
+{
+	public class BudgetModel
+	{
+		public string Description { get; set; }
+		public decimal Value { get; set; }
+	}
+}
